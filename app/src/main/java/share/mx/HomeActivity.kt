9f9753a1b05268ab392dcf204d1a712ac6db0e7f
devgatito <eliasmisaelsotojimenez1@gmail.com>
2026-05-02 12:56:50 -0600

    integrando files

--- app/src/main/java/share/mx/HomeActivity.kt
+++ app/src/main/java/share/mx/HomeActivity.kt
@@ -2,6 +2,9 @@ package share.mx
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -9,9 +12,15 @@ class HomeActivity : AppCompatActivity() {
 
     private lateinit var bottomNav: BottomNavigationView
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContentView(R.layout.activity_home)
+
+        hideSystemUI()
 
         bottomNav = findViewById(R.id.bottomNav)
 
@@ -40,6 +49,20 @@ class HomeActivity : AppCompatActivity() {
                 else -> false
             }
         }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            hideSystemUI()
+        }
+    }
+
+    private fun hideSystemUI() {
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        controller.hide(WindowInsetsCompat.Type.systemBars())
     }
 
     private fun loadFragment(fragment: Fragment) {
